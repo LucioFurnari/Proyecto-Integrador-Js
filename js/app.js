011730c74fdbe88b87e0eecae7f56e3cb7a83cf5
LucioFurnari <luciofurnari99@gmail.com>
2022-07-17 19:08:47 -0300

    cambios en style

--- js/app.js
+++ js/app.js
@@ -2,25 +2,27 @@ import { Products, obj, products } from "./bd.js";
 import { itemFilter, createItems, FilterInput } from "./createItem.js";
 import { cleanHTML } from "./cleanHTML.js";
 
-export const shopContainerGrid = document.querySelector(".container-shop"),
+export const shopContainerGrid = document.querySelector(".container-shop"), // DOM Elements //
     shopFilter = document.querySelector(".buttons-filter"),
     shopBtnShow = document.querySelector(".btn-showshop"),
-    inputFilter = document.querySelector(".input-filter");
+    inputFilter = document.querySelector(".input-filter"),
+    cartButton = document.querySelector(".cart-btn");
     
 
-window.addEventListener("DOMContentLoaded",createItems(products,shopContainerGrid));
-
+window.addEventListener("DOMContentLoaded",createItems(products,shopContainerGrid));// Crea items del shop //
 const shopItems = document.querySelectorAll(".item"); // Guarda los items del shop //
-shopFilter.addEventListener("click",(event) =>{
+
+shopFilter.addEventListener("click",(event) =>{ // Filtra los productos con los botones //
     itemFilter(shopItems, event.target.value);
     console.log(event.target.value);
 })
-inputFilter.addEventListener("input",(e) => {
+
+inputFilter.addEventListener("input",(e) => { // Filtra los productos con el input //
     FilterInput(shopItems,e.target.value)
     console.log(e.target.value);
 })
-console.log(shopItems[1].children[0].textContent);
 
+console.log(shopItems[1].children[0].textContent);
 // console.log(shopItems);
 
 
