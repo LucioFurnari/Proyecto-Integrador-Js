fe60f51dff22cb831f3bf761272b0e0a82cf41a7
LucioFurnari <luciofurnari99@gmail.com>
2022-07-17 18:36:07 -0300

    Creando filter input, agregando iconos

--- js/app.js
+++ js/app.js
@@ -1,23 +1,26 @@
 import { Products, obj, products } from "./bd.js";
-import { itemFilter, createItems } from "./createItem.js";
+import { itemFilter, createItems, FilterInput } from "./createItem.js";
 import { cleanHTML } from "./cleanHTML.js";
 
 export const shopContainerGrid = document.querySelector(".container-shop"),
     shopFilter = document.querySelector(".buttons-filter"),
-    shopBtnShow = document.querySelector(".btn-showshop");
+    shopBtnShow = document.querySelector(".btn-showshop"),
+    inputFilter = document.querySelector(".input-filter");
     
 
 window.addEventListener("DOMContentLoaded",createItems(products,shopContainerGrid));
-// shopBtnShow.addEventListener("click",() => {
-//     cleanHTML(shopContainerGrid)
-//     createItems(products,shopContainerGrid)})
+
 const shopItems = document.querySelectorAll(".item"); // Guarda los items del shop //
 shopFilter.addEventListener("click",(event) =>{
     itemFilter(shopItems, event.target.value);
     console.log(event.target.value);
 })
+inputFilter.addEventListener("input",(e) => {
+    FilterInput(shopItems,e.target.value)
+    console.log(e.target.value);
+})
+console.log(shopItems[1].children[0].textContent);
 
-
-console.log(shopItems);
+// console.log(shopItems);
 
 
--- js/createItem.js
+++ js/createItem.js
@@ -19,28 +19,42 @@ export function createItems(arr, elem){ // Crea card de los productos //
         item.setAttribute("category",arr[i].category);
         item.classList.add("item");
         item.innerHTML = `
-            <h3>${arr[i].name}</h3>
-            <img src=${arr[i].img}>
-            <p class="item-description">${arr[i].description}</p>
-            <span>${arr[i].price} $</span>
-            <button class="buy-btn">Comprar</button>
-            `
+        <h3>${arr[i].name}</h3>
+        <img src=${arr[i].img}>
+        <p class="item-description">${arr[i].description}</p>
+        <span>${arr[i].price} $</span>
+        <button class="buy-btn">Comprar</button>
+        `
         elem.appendChild(item);
     }
 }
 
 export function itemFilter(node,val){ // Filtra los productos del shop a travez de los botones //
-    for(let j=0; j<node.length;j++){
-        node[j].classList.add("item");
-        node[j].classList.remove("item-hide");
-        if(node[j].getAttribute("category") != val){
-            node[j].classList.remove("item");
-            node[j].classList.toggle("item-hide");
-        }
-        if(val == "shop"){
+    if(val != undefined){
+        for(let j=0; j<node.length;j++){
             node[j].classList.add("item");
             node[j].classList.remove("item-hide");
+            if(node[j].getAttribute("category") != val){
+                node[j].classList.remove("item");
+                node[j].classList.toggle("item-hide");
+            }
+            if(val == "shop"){
+                node[j].classList.add("item");
+                node[j].classList.remove("item-hide");
+            }
+            console.log(node[j].getAttribute("category"));
         }
-        console.log(node[j].getAttribute("category"));
     }
+}
+
+export function FilterInput(node,val){
+    for(let l=0; l<node.length; l++){
+        node[l].classList.toggle("item");
+        node[l].classList.add("item-hide");
+        if(node[l].children[0].textContent.includes(val)){
+            node[l].classList.add("item");
+            node[l].classList.remove("item-hide");
+        }
+    }
+    
 }
