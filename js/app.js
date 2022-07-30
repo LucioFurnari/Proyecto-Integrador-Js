a801bd31eeb9898969f17d5321c7f84f5e0cb4f2
LucioFurnari <luciofurnari99@gmail.com>
2022-07-30 19:06:17 -0300

    Cambios

--- js/app.js
+++ js/app.js
@@ -7,8 +7,7 @@ export const shopContainerGrid = document.querySelector(".container-shop"), // D
     shopFilter = document.querySelector(".buttons-filter"),
     shopBtnShow = document.querySelector(".btn-showshop"),
     inputFilter = document.querySelector(".input-filter"),
-    shopItems = document.querySelectorAll(".item"), // Guarda los items del shop //
-    itemQntCart = document.querySelectorAll(".cart-item input");
+    shopItems = document.querySelectorAll(".item"); // Guarda los items del shop //
     
 
 window.addEventListener("DOMContentLoaded",() => {
@@ -22,6 +21,37 @@ window.addEventListener("DOMContentLoaded",() => {
     })
 
     totalPrice(compraArray)// Precio total se muestra al iniciar la pagina
+
+    const itemQntCart = document.querySelectorAll(".cart-item input");
+    console.log(itemQntCart);
+    itemQntCart.forEach(item => {
+        item.addEventListener("change", (e) => {
+            // console.log(item.parentElement.firstElementChild.textContent);
+            let name = item.parentElement.firstElementChild.textContent;
+            let value = parseInt(e.target.value);
+            compraArray.map(elem => {
+                if(elem.name == name){
+                    elem.qnt = value;
+                }
+                localStorage.setItem("Compra",JSON.stringify(compraArray))
+            })
+            totalPrice(compraArray)// Precio total se muestra al iniciar la pagina
+            // console.log(e.target.value);
+        })
+    })
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
 });
 
 ///// Boton del carrito /////
@@ -56,24 +86,6 @@ inputFilter.addEventListener("input",(e) => { // Filtra los productos con el inp
     
     // compraArray.forEach(obj => totalPrice(obj));// Precio total
     
-    
-    itemQntCart.forEach(item => {
-        item.addEventListener("change", (e) => {
-            // console.log(item.parentElement.firstElementChild.textContent);
-            let name = item.parentElement.firstElementChild.textContent;
-            let value = parseInt(e.target.value);
-            compraArray.map(elem => {
-                if(elem.name == name){
-                    elem.qnt = value;
-                }
-                localStorage.setItem("Compra",JSON.stringify(compraArray))
-            })
-            
-            // console.log(e.target.value);
-        })
-    })
-    console.log(itemQntCart);
-    
-    
+   
     
 
