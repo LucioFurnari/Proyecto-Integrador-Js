0a3df5460704737128dd28a4805498fb61cbe059
LucioFurnari <luciofurnari99@gmail.com>
2022-08-02 11:57:38 -0300

    Agregando imagenes, cambiando style.scss

--- js/bd.js
+++ js/bd.js
@@ -1,8 +1,8 @@
 
 export const products = [
-    {name:"cafe 1", description:"lalala",price:150, img:"", category:"cafe", id:1},
-    {name:"cafe 2", description:"lalala",price:170, img:"", category:"cafe", id:2},
-    {name:"cafe 3", description:"lalala",price:120, img:"", category:"cafe", id:3},
+    {name:"Espresso", description:"lalala",price:150, img:"./assets/images/espresso.jpg", category:"cafe", id:1},
+    {name:"Mocca", description:"lalala",price:170, img:"./assets/images/mocca.jpg", category:"cafe", id:2},
+    {name:"Latte", description:"lalala",price:120, img:"./assets/images/latte.jpg", category:"cafe", id:3},
     {name:"cafe 4", description:"lalala",price:100, img:"", category:"cafe", id:4},
     {name:"cafe 5", description:"lalala",price:125, img:"", category:"cafe", id:5},
     {name:"chocolate 1", description:"lalala",price:100, img:"", category:"chocolate", id:6},
